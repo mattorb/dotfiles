b8c637d9064f3b8e4ea7a99ee9b8e79f38d586b6
Matthew Smith <msmith@msmith.fios-router.home>
2017-10-03 22:09:04 -0500

    Do not symlink fish config dirs we do not need yet

--- symlink-common.sh
+++ symlink-common.sh
@@ -3,8 +3,4 @@
 ln -sf $(pwd)/git/.gitaliases $HOME/.gitaliases
 
 mkdir -p $HOME/.config/fish
-ln -sf $(pwd)/fish/completions "$HOME/.config/fish"
 ln -sf $(pwd)/fish/functions "$HOME/.config/fish"
-ln -sf $(pwd)/fish/config.fish "$HOME/.config/fish/config.fish"
-ln -sf $(pwd)/fish/aliases.fish "$HOME/.config/fish/aliases.fish"
-
