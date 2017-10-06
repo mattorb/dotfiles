e148bfd22787ae95b62c6d4059b3657d96686d00
Matthew Smith <msmith@msmith.fios-router.home>
2017-10-05 22:18:52 -0500

    Add bin symlink and add to system path

--- symlink-common.sh
+++ symlink-common.sh
@@ -4,3 +4,6 @@ ln -sf $(pwd)/git/.gitaliases $HOME/.gitaliases
 
 mkdir -p $HOME/.config/fish
 ln -sf $(pwd)/fish/functions "$HOME/.config/fish"
+ln -sf $(pwd)/fish/config.fish "$HOME/.config/fish/config.fish"
+
+ln -sf $(pwd)/bin $HOME
