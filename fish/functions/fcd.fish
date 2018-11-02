486a0671dad0ae54ebe4dec2b85be2c90d53e974
mattorb <matt@piikeastreet.com>
2018-11-02 07:51:35 -0500

    Fuzzy change directory w/fish + fzf
    Execute 'fcd', then auto-complete type any directory on disk, and
    press enter to go there.
    
    source: https://gist.github.com/rumpelsepp/b1b416f52d6790de1aee

---
+++ fish/functions/fcd.fish
@@ -0,0 +1,19 @@
+function fcd -d "Fuzzy change directory"
+    if set -q argv[1]
+        set searchdir $argv[1]
+    else
+        set searchdir $HOME
+    end
+
+    # https://github.com/fish-shell/fish-shell/issues/1362
+    set -l tmpfile (mktemp)
+    find $searchdir \( ! -regex '.*/\..*' \) ! -name __pycache__ -type d | fzf > $tmpfile
+    set -l destdir (cat $tmpfile)
+    rm -f $tmpfile
+
+    if test -z "$destdir"
+        return 1
+    end
+
+    cd $destdir
+end
