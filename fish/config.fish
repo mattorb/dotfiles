1595e790a9eb2d59fdfc4fcc2b847671389b5d37
mattorb <matt@piikeastreet.com>
2019-01-23 07:49:01 -0600

    Add a 10 line preview window to top of screen for FZF reverse history search
    Mostly to make it easier to see those multi-line docker commands pasted
    from runbooks/etc.

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,5 @@ function fish_user_key_bindings
     set -U FZF_LEGACY_KEYBINDINGS 0
     source $HOME/.config/fish/conf.d/fzf_key_bindings.fish
 end
+
+set -x FZF_REVERSE_ISEARCH_OPTS '--preview-window=up:10 --preview="echo {}" --height 100%'
