4d6da8c467b07dca2b130d6a71ea48fe3a8f19e2
mattorb <matt@piikeastreet.com>
2019-04-29 08:32:53 -0500

    Add ripgrep for insanely fast searching over git repos with sane defaults

--- install-osx-dev-apps.sh
+++ install-osx-dev-apps.sh
@@ -42,7 +42,8 @@ brew install \
     parallel \
     telnet \ 
     netcat \
-    Tenzer/tap/multitime
+    Tenzer/tap/multitime \ 
+    ripgrep
 
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
 
