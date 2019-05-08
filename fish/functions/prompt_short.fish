1e62924b215fc959a65f5a69126fde391ce1b913
mattorb <matt@piikeastreet.com>
2019-05-07 21:39:39 -0500

    Add short prompt switcher

---
+++ fish/functions/prompt_short.fish
@@ -0,0 +1,3 @@
+function prompt_short --description 'Short prompt.  Drops right prompt/etc for easier copy/paste, screen recording, etc, but keeps the directory on the left'
+	source ~/.config/fish/functions/ancillary/prompt_short.fish
+end
