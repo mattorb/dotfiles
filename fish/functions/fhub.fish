71f34b6ca76fb61fc8792e45c3e9b5a32ab5887e
mattorb <matt@piikeastreet.com>
2019-05-29 08:35:59 -0500

    Pull out a common driver for fclone and fhub into fgithubrepo
    They were 95% the same.  With all the caching logic it's starting
    to make sense to maintain this in one place.

--- fish/functions/fhub.fish
+++ fish/functions/fhub.fish
@@ -1,32 +1,10 @@
 # Offers an fzf chooser to pop open a repo's github home page in the browser
 function fhub -d 'Choose a repo to open github homepage for.  Put personal access token in $HOME/.fhub_token and additional orgs in $HOME/.fhub_orgs'
-    if not test -e "$HOME/.fhub_token"
-        echo "Put github personal access token in $HOME/.fhub_token"
-        return 1
-    end
-    
-    if test -e "$HOME/.fhub_token"
-        read -z ADD_ORGS < $HOME/.fhub_orgs
-    end 
+    fgithubrepo browsegithubrepo
+end
 
-    set TOKEN (cat $HOME/.fhub_token)
-    set ARG_SHA (echo $TOKEN $ADD_ORGS | shasum -a 512 | awk '{print $1}')
-    set CACHE ~/.fhub_repo_list_cache.txt-$ARG_SHA
-    set ITEMS (cat $CACHE 2>/dev/null| wc -l)
-
-    # Immediately send cache records to FZF, and kick off refreshing the cache via listrepo . . . piping that to FZF as we receive.
-    #  nauniq dedupes it for fzf.  It's critical that output from the first thing not wait on the second to start populating FZF UI, thus the custom fifo.
-    set FIFO (mktemp -ut tem)
-    rm -f $FIFO
-    mkfifo $FIFO
-    cat $CACHE 2>/dev/null > $FIFO &
-    listrepo_gql (cat $HOME/.fhub_token) $ADD_ORGS | tee $CACHE > $FIFO &
-    cat $FIFO | nauniq | fzf | read -l repo
-
-    if test -n "$repo"
-        echo "Opening '$repo' in Github"
-        hub browse $repo
-    end
-
-    rm $FIFO
+function browsegithubrepo
+    set REPO "$argv[1]"
+    echo Opening $REPO in browser
+    hub browse $REPO
 end
