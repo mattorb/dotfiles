fa50e19c8c13eafd1ba1fc015d5dd86661804f07
mattorb <matt@piikeastreet.com>
2019-05-30 07:44:26 -0500

    Name var for purpose rather than type and improve docs

--- fish/functions/fgithubrepo.fish
+++ fish/functions/fgithubrepo.fish
@@ -19,26 +19,26 @@ function fgithubrepo -d 'Present an fzf chooser for a github repo.  Put personal
     set CACHE ~/.fhub_repo_list_cache.txt-$ARG_SHA
     
     # Create a queue to feed FZF with
-    set FIFO (mktemp -ut fgithubrepo_cache)
-    mkfifo $FIFO
+    set QUEUE (mktemp -ut fgithubrepo_cache)
+    mkfifo $QUEUE
 
-    # Async feed persisted cache results to the fifo
-    cat $CACHE 2>/dev/null > $FIFO &
+    # Async, get in line to feed persisted cache results to the queue
+    cat $CACHE 2>/dev/null > $QUEUE &
 
     # Async fetch repo list via github API
-    # Send results in parallel (tee) to:  1. Persisted cache. 2. queue (fifo)
-    listrepo_gql (cat $HOME/.fhub_token) $ADD_ORGS | tee $CACHE > $FIFO &
+    # Get in line to send results in parallel (tee) to:  1. Persisted cache. 2. queue (fifo)
+    listrepo_gql (cat $HOME/.fhub_token) $ADD_ORGS | tee $CACHE > $QUEUE &
 
     # Feed the queue to fzf, de-duping (w/nauniq) since persisted cache is likely to have heavy overlap with realtime results 
     #   if the cache was populated.
-    cat $FIFO | nauniq | fzf | read -l repo
+    cat $QUEUE | nauniq | fzf | read -l repo
      
-    # Serial example.  Gets stuck on waiting for both to finish before sending anything to fzf
+    # Serial example w/o fifo queue.  Gets stuck on waiting for both to finish before sending anything to fzf
     #cat $CACHE 2>/dev/null; listrepo_gql (cat $HOME/.fhub_token) $ADD_ORGS | tee $CACHE | fzf | read -l repo
 
     if test -n "$repo"
         $COMMAND $repo
     end
 
-    rm $FIFO
+    rm $QUEUE
 end
