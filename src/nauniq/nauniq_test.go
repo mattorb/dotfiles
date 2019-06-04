7ab722572ac63782d7f085cb67d32c5459b16dc0
mattorb <matt@piikeastreet.com>
2019-06-03 22:21:31 -0500

    nauniq: More succinct table test
    Let the loop over the table test do the work of preparing the
    data structure that represents state (map of string to empty structs),
    avoiding having to declare empty structs throughout table test.

--- src/nauniq/nauniq_test.go
+++ src/nauniq/nauniq_test.go
@@ -9,29 +9,35 @@ func TestUniqueStrings(t *testing.T) {
 		s string
 	}
 
-	type lineset map[string]struct{}
-
 	tests := []struct {
-		name    string
-		entries lineset
-		arg     string
-		added   bool
+		name      string
+		pastlines []string
+		testline  string
+		new       bool
 	}{
-		{"No Entries - Add one", lineset{}, "test", true},
-		{"Single Entry - Add dupe", lineset{"test": {}}, "test", false},
-		{"Multiple Entries - adjacent tail dupe", lineset{"test": {}, "test2": {}}, "test2", false},
-		{"Multiple Entries - non-adjacent head dupe", lineset{"test": {}, "test2": {}}, "test", false},
-		{"Multiple Entries - non-adjacent dupe in middle", lineset{"test": {}, "test2": {}, "test3": {}}, "test2", false},
+		{"No Entries - Add one", []string{}, "test", true},
+		{"Single Entry - Add dupe", []string{"test"}, "test", false},
+		{"Multiple Entries - adjacent tail dupe", []string{"test", "test2"}, "test2", false},
+		{"Multiple Entries - non-adjacent head dupe", []string{"test", "test2"}, "test", false},
+		{"Multiple Entries - non-adjacent dupe in middle", []string{"test", "test2", "test3"}, "test2", false},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			dt := &UniqueStrings{
-				entries: tt.entries,
+			us := &UniqueStrings{
+				entries: sliceToMap(tt.pastlines),
 			}
-			if got := dt.add(tt.arg); got != tt.added {
-				t.Errorf("DupeTracker.track() = %v, expected %v", got, tt.added)
+			if got := us.add(tt.testline); got != tt.new {
+				t.Errorf("UniqueStrings.add() = %v, expected %v", got, tt.new)
 			}
 		})
 	}
+}
+
+func sliceToMap(elements []string) map[string]struct{} {
+	elementMap := make(map[string]struct{})
+	for _, s := range elements {
+		elementMap[s] = struct{}{}
+	}
+	return elementMap
 }
