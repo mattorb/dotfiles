f5382fa9f9d332ca507ea17daf21bb4679bb8ee9
mattorb <matt@piikeastreet.com>
2019-07-17 08:25:55 -0500

    Add beginning/end of buffer shortcuts
    Also, vertically compress this so it doesn't scroll off my screen
    when alt-K'ing inline

--- fish/functions/keys.fish
+++ fish/functions/keys.fish
@@ -1,15 +1,18 @@
 function keys -d "Help for bound keys"
-    echo Alt-K\tShow this help
+    echo Alt-K\tShow this help keeping current command line
+    echo
 
-    echo Line Level
+    echo Buffer/Line
     echo --------------------------------------------------
-    echo Ctrl-A\t Move Cursor to beginning line 
-    echo Ctrl-E\t Move Cursor to end of line
+    echo Alt-\<\t Move cursor to beginning of Buffer
+    echo Alt-\>\t Move cursor to end of buffer
+    echo Ctrl-A\t Move cursor to beginning line 
+    echo Ctrl-E\t Move cursor to end of line
     echo Ctrl-U\t Delete to beginning of line \(to killring\)
     echo Ctrl-K\t Delete to end of line \(to killlring\)
     echo 
 
-    echo Word Level
+    echo Word, Character, killring, history
     echo --------------------------------------------------
     echo Ctrl-W\t Delete back a word \(to killring\)
     echo Alt-D\t Delete forward word \(to killring\)
@@ -18,24 +21,14 @@ function keys -d "Help for bound keys"
     echo Alt-U\t Uppercase current word
     echo Option-Left\t Move cursor to next word
     echo Option-Right\t Move cursor to previous word
-    echo Alt-L list contents of directory under cursor
-
-    echo Character level
-    echo --------------------------------------------------
+    echo Alt-L\t list contents of directory under cursor
     echo Ctrl-T\t Transpose last two characters
     echo Fn-Del\t Forward Delete a character
-    echo
-
-    echo killring
-    echo --------------------------------------------------
     echo Ctrl-Y\t Paste last killring entry
     echo Alt-Y\t Rotate back a killring entry
-    echo
-
-    echo history
-    echo --------------------------------------------------
-    echo Alt-Shift-Left/Right On empty command line, navigate directory history
+    echo Alt-Shift-Left/Right\t On empty command line, navigate directory history
     echo Alt-Up/Down\t Search history for token at cursor     \(fish\)
+    echo
 
     echo fzf fish add-ons
     echo ---------------------------------------------------------
