547488ab3da51d8d052efa08a4f1611cbf818629
mattorb <matt@piikeastreet.com>
2019-08-14 22:14:47 -0500

    A basic hammerspoon setup
    Depend on SpoonInstall for pulling in new remote 'Spoons'
    
    Trying out MenuHammer from https://github.com/FryJay/MenuHammer

---
+++ hammerspoon/init.lua
@@ -0,0 +1,18 @@
+hs.loadSpoon("SpoonInstall")
+spoon.SpoonInstall.use_syncinstall = true
+Install=spoon.SpoonInstall
+
+hyper = {"cmd", "alt", "ctrl", "shift"}
+
+spoon.SpoonInstall:installSpoonFromZipURL('https://github.com/mattorb/MenuHammer/raw/master/Spoons/MenuHammer.spoon.zip')
+menuHammer = hs.loadSpoon("MenuHammer")
+menuHammer:enter()
+
+--
+-- Useful when testing changes: reload hammerspoon config
+--
+-- hs.hotkey.bind(hyper, "R", function()
+--   hs.reload()
+-- end)
+
+hs.alert.show("Config loaded")
