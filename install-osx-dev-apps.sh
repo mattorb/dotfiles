348fc5aea6cf9bdeecfe7ce41f8595ef0ea03334
mattorb <matt@piikeastreet.com>
2019-09-09 21:09:08 -0500

    Fix syntax error

--- install-osx-dev-apps.sh
+++ install-osx-dev-apps.sh
@@ -56,7 +56,7 @@ brew "bash"
 EOS
 
 echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
-sudo -v || echo Prompt for pass or keep going (Github Action mode)
+sudo -v || echo Prompt for pass or keep going -- likely running at a Github Action
 sudo chsh -s /usr/local/bin/fish $(whoami)
 
 # fisher for completions. 3.2.7
