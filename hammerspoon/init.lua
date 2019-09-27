b2e6ccf9ebf47772c5fc11fd49675eb02a6a4390
mattorb <matt@piikeastreet.com>
2019-09-27 08:49:04 -0500

    Hyper-o to search application menu items and pick one w/hammerspoon chooser
    Disabled menu item cache per app.  Loads items on-demand when hotkey is
    pressed which is sometimes slow.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -18,15 +18,6 @@ emojis = hs.loadSpoon('Emojis')
 emojis.chooser:rows(15)
 emojis:bindHotkeys({toggle={hyper, 'e'}})
 
---
--- Useful when testing changes: reload hammerspoon config
---
--- hs.hotkey.bind(hyper, "R", function()
---   hs.reload()
--- end)
-
--- hs.alert.show("Hammerspoon config loaded")
-
 require('keyboard')
 
 -- working around some new os x nuance where this preference is not successfully preserved in the plist
@@ -34,3 +25,5 @@ require('keyboard')
 if hs.dockicon.visible() then
     hs.dockicon.hide()
 end
+
+require('menusearch')
