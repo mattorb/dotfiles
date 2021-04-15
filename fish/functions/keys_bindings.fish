ea0216d44db22099a0aefbce9f643ab0c0a10bdc
mattorb <matt@piikeastreet.com>
2021-04-14 22:40:02 -0500

    Kaleidoscope/Chrysalis: Atreus on-screen layer help via Alt-0,1,2,3 from cli

--- fish/functions/keys_bindings.fish
+++ fish/functions/keys_bindings.fish
@@ -1,1 +1,7 @@
 bind --preset $argv \ek 'begin; echo; keys; commandline -f repaint; end'
+
+# Atreus on-screen layer help from cli
+# Alt-1 to show keyboard layer 1 help, etc
+for x in (string split ' ' '0 1 2 3')
+    bind --preset $argv \e$x "begin; qlmanage -p ~/.config/kaleidoscope_with_chrysalis/layer$x.png 1>/dev/null 2>/dev/null; end"
+end
