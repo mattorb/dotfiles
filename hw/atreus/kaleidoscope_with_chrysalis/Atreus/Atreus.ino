4644a95037bf0f83c75d4901685a46c1fbd4b31f
mattorb <matt@piikeastreet.com>
2021-06-04 23:50:52 -0500

    Atreus layout changes more closely mirror modifier layout of traditional keyboard

--- hw/atreus/kaleidoscope_with_chrysalis/Atreus/Atreus.ino
+++ hw/atreus/kaleidoscope_with_chrysalis/Atreus/Atreus.ino
@@ -67,12 +67,12 @@ KEYMAPS(
        Key_Q   ,Key_W   ,Key_E       ,Key_R         ,Key_T
       ,Key_A   ,Key_S   ,Key_D       ,Key_F         ,Key_G
       ,Key_Z   ,Key_X   ,Key_C       ,Key_V         ,Key_B, Key_Backtick
-      ,Key_Esc ,Key_Tab ,Key_LeftGui ,Key_LeftShift ,Key_Backspace ,Key_LeftControl
+      ,Key_Esc ,Key_LeftShift, Key_LeftControl, Key_LeftAlt, Key_LeftGui, Key_Backspace 
 
                      ,Key_Y     ,Key_U      ,Key_I     ,Key_O      ,Key_P
                      ,Key_H     ,Key_J      ,Key_K     ,Key_L      ,Key_Semicolon
        ,Key_Backslash,Key_N     ,Key_M      ,Key_Comma ,Key_Period ,Key_Slash
-       ,Key_LeftAlt  ,Key_Space ,MO(FUN)    ,Key_Minus ,Key_Quote  ,Key_Enter
+       ,Key_Tab, Key_Space ,MO(FUN)    ,Key_Minus ,Key_Quote  ,Key_Enter
   ),
 
   [FUN] = KEYMAP_STACKED
