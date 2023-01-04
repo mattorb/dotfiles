4e0be110191168b5d68a5d32de335fdf4908aeaf
Arjay Gallentes <agallentes@SGGMBP-15i19.local>
2023-01-04 21:09:06 +0800

    Add script folder, and function script

---
+++ scripts/functions.sh
@@ -0,0 +1,73 @@
+#! /usr/bin/env sh
+
+symlink() {
+    OVERWRITTEN=""
+    if [ -e "$2" ] || [ -h "$2" ]; then
+        OVERWRITTEN="(Overwritten)"
+        if ! rm -r "$2"; then
+            substep_error "Failed to remove existing file(s) at $2."
+        fi
+    fi
+    if ln -s "$1" "$2"; then
+        substep_success "Symlinked $2 to $1. $OVERWRITTEN"
+    else
+        substep_error "Symlinking $2 to $1 failed."
+    fi
+}
+
+clear_broken_symlinks() {
+    find -L "$1" -type l | while read fn; do
+        if rm "$fn"; then
+            substep_success "Removed broken symlink at $fn."
+        else
+            substep_error "Failed to remove broken symlink at $fn."
+        fi
+    done
+}
+
+# Took these printing functions from https://github.com/Sajjadhosn/dotfiles
+coloredEcho() {
+    local exp="$1";
+    local color="$2";
+    local arrow="$3";
+    if ! [[ $color =~ '^[0-9]$' ]] ; then
+       case $(echo $color | tr '[:upper:]' '[:lower:]') in
+        black) color=0 ;;
+        red) color=1 ;;
+        green) color=2 ;;
+        yellow) color=3 ;;
+        blue) color=4 ;;
+        magenta) color=5 ;;
+        cyan) color=6 ;;
+        white|*) color=7 ;; # white or invalid color
+       esac
+    fi
+    tput bold;
+    tput setaf "$color";
+    echo "$arrow $exp";
+    tput sgr0;
+}
+
+info() {
+    coloredEcho "$1" blue "========>"
+}
+
+success() {
+    coloredEcho "$1" green "========>"
+}
+
+error() {
+    coloredEcho "$1" red "========>"
+}
+
+substep_info() {
+    coloredEcho "$1" magenta "===="
+}
+
+substep_success() {
+    coloredEcho "$1" cyan "===="
+}
+
+substep_error() {
+    coloredEcho "$1" red "===="
+}
